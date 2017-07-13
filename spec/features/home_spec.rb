65fedbb9dd464794f900a5f9cfa426de4276f860
Andrew Menning <menning23@gmail.com>
2017-07-12 20:13:45 -0400

    update home page text

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'visiting the homepage' do
   scenario 'the visitor sees welcome text' do
     visit root_path
-    expect(page).to have_text('Welcome to RubyThursday')
+    expect(page).to have_text('Welcome to the Playground')
   end
 end
